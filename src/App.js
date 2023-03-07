6e029d93dc7d3bbfcc741385ab492afc05024209
Eliswer <106431426+Eliswer@users.noreply.github.com>
2023-03-07 16:47:36 +0100

    fetch data on load

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./css/App.css";
 import citySearch from "./apiCurrentWeather";
 import WeatherData from "./WeatherData";
 import dailyWeather from "./apiDailyWeather";
 
 function App() {
-  const [cityName, setCityName] = useState("Prague");
-  const [icon, setIcon] = useState();
+  useEffect(() => {
+    fetchFirstData();
+  }, []);
+
+  const [cityName, setCityName] = useState("");
+  const [dailyResponse, setDailyResponse] = useState();
   const [showWeather, setShowWeather] = useState({
     degrees: 0,
     place: ["Prague, CZ"],
     clouds: "cloudy",
+    icon: "",
     unixTimestamp: "Monday, March 6, 2023 9:10:08 PM",
   });
+
+  const fetchFirstData = async () => {
+    const firstResponse = await citySearch(cityName);
+
+    setShowWeather({
+      ...showWeather,
+      degrees: firstResponse.data.main.temp,
+      place: [firstResponse.data.name, firstResponse.data.sys.country],
+      icon: firstResponse.data.weather[0].icon,
+      clouds: firstResponse.data.weather[0].description,
+      unixTimestamp: firstResponse.data.dt,
+    });
+  };
 
   const getData = async (event) => {
     event.preventDefault();
 
     const response = await citySearch(cityName);
-    const dailyResponse = await dailyWeather(cityName);
-
-    setIcon(response.data.weather[0].icon);
-    console.log(icon);
+    setDailyResponse(await dailyWeather(cityName));
 
     setShowWeather({
       ...showWeather,
       degrees: response.data.main.temp,
       place: [response.data.name, response.data.sys.country],
+      icon: response.data.weather[0].icon,
       clouds: response.data.weather[0].description,
       unixTimestamp: response.data.dt,
     });
@@ -51,8 +67,8 @@ function App() {
 
       <WeatherData
         showWeather={showWeather}
-        icon={icon}
-        //dailyResponse={dailyResponse}
+        dailyResponse={dailyResponse}
+        icon={showWeather.icon}
       />
     </div>
   );

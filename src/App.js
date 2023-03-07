52df61998bcff8a5f77c8efbfa92df8eb85fbcce
Eliswer <106431426+Eliswer@users.noreply.github.com>
2023-03-07 18:33:34 +0100

    s

--- src/App.js
+++ src/App.js
@@ -5,17 +5,32 @@ import WeatherData from "./WeatherData";
 
 function App() {
   const [cityName, setCityName] = useState("Prague");
+<<<<<<< HEAD
 
+=======
+  const [icon, setIcon] = useState();
+>>>>>>> parent of 6e029d9 (fetch data on load)
   const [showWeather, setShowWeather] = useState({
     degrees: 0,
     place: ["Prague, CZ"],
     clouds: "cloudy",
+<<<<<<< HEAD
+=======
+    unixTimestamp: "Monday, March 6, 2023 9:10:08 PM",
+>>>>>>> parent of 6e029d9 (fetch data on load)
   });
 
   const getData = async (event) => {
     event.preventDefault();
 
     const response = await citySearch(cityName);
+<<<<<<< HEAD
+=======
+    const dailyResponse = await dailyWeather(cityName);
+
+    setIcon(response.data.weather[0].icon);
+    console.log(icon);
+>>>>>>> parent of 6e029d9 (fetch data on load)
 
     setShowWeather({
       ...showWeather,
@@ -42,7 +57,15 @@ function App() {
         <button onClick={getData}>Search</button>
       </form>
 
+<<<<<<< HEAD
       <WeatherData showWeather={showWeather} cityName={cityName} />
+=======
+      <WeatherData
+        showWeather={showWeather}
+        icon={icon}
+        //dailyResponse={dailyResponse}
+      />
+>>>>>>> parent of 6e029d9 (fetch data on load)
     </div>
   );
 }
--- src/WeatherData.jsx
+++ src/WeatherData.jsx
@@ -9,12 +9,28 @@ function WeatherData({ showWeather, cityName }) {
   };
 
   return (
+<<<<<<< HEAD
     <div className="weather-data flex-center">
       <div className="weather-data-top">
         <p className="data-city">
           {showWeather.place[0]}, {showWeather.place[1]}
         </p>
         <p className="data-time"></p>
+=======
+    <>
+      <div className="weather-data flex-center">
+        <div className="weather-data-top">
+          <p className="data-city">
+            {showWeather.place[0]}, {showWeather.place[1]}
+          </p>
+          <p className="data-time">
+            {yearlyTime} {time}
+          </p>
+          <p className="data-weather">{showWeather.clouds}</p>
+        </div>
+        <img src={`Weather-app/src/img/icons/${icon}@2x.png`} alt="icon"></img>
+        <h1 className="data-degrees">{showWeather.degrees}°C</h1>
+>>>>>>> parent of 6e029d9 (fetch data on load)
       </div>
       <h1 className="data-degrees">{showWeather.degrees}°C</h1>
       <div className="weather-data-bottom">

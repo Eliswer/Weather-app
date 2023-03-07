b6da6e6e0060bf4b11c4e38b1c6c9296bf0dc1e5
Eliswer <106431426+Eliswer@users.noreply.github.com>
2023-03-07 18:53:08 +0100

    ++

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./css/App.css";
 import citySearch from "./apiCurrentWeather";
 import WeatherData from "./WeatherData";
 import dailyWeather from "./apiDailyWeather";
 
 function App() {
+  useEffect(() => {
+    fetchFirstData();
+  }, []);
+
   const [cityName, setCityName] = useState("Prague");
-  const [icon, setIcon] = useState();
   const [showWeather, setShowWeather] = useState({
     degrees: 0,
     place: ["Prague, CZ"],
     clouds: "cloudy",
+    icon: "",
     unixTimestamp: "Monday, March 6, 2023 9:10:08 PM",
   });
+
+  const fetchFirstData = async () => {
+    const firstResponse = await citySearch(cityName);
+
+    setShowWeather({
+      ...showWeather,
+      degrees: firstResponse.data.main.temp,
+      place: [firstResponse.data.name, firstResponse.data.sys.country],
+      icon: firstResponse.data.weather[0].icon,
+      clouds: firstResponse.data.weather[0].description,
+      unixTimestamp: firstResponse.data.dt,
+    });
+    console.log(showWeather);
+  };
 
   const getData = async (event) => {
     event.preventDefault();
 
     const response = await citySearch(cityName);
-    const dailyResponse = await dailyWeather(cityName);
-
-    setIcon(response.data.weather[0].icon);
-    console.log(icon);
 
     setShowWeather({
       ...showWeather,
@@ -51,7 +65,7 @@ function App() {
 
       <WeatherData
         showWeather={showWeather}
-        icon={icon}
+        icon={showWeather.icon}
         //dailyResponse={dailyResponse}
       />
     </div>
